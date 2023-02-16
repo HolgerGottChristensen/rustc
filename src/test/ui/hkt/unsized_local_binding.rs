1b1c4c839747b861f28401870291e065f22c1390
holgerchristensen <holger.gott@icloud.com>
2023-02-16 21:21:06 +0100

    Test that local parameters are checked for Sized.

--- src/test/ui/hkt/unsized_local_binding.rs
+++ src/test/ui/hkt/unsized_local_binding.rs
@@ -1,8 +1,7 @@
-// HKT test
+// HKT The local binding should report that I<[u32]> is not sized.
 
 fn test<I<%J>>() {
-    let i: I<[u32]> = todo!();
-    // ~^ ERROR `[u32]` is not Sized
+    let i: I<[u32]> = todo!(); //~ ERROR the size for values of type `[u32]` cannot be known at compilation time [E0277]
 }
 
 fn main() {}
