b3a45e9eefb36a740ff1fbef64397da2f64ebaf1
muki <muki@itu.dk>
2023-05-23 12:56:12 +0200

    - tests

--- src/test/ui/hkt/inference_nested_hkt_param_many_solutions_error.rs
+++ src/test/ui/hkt/inference_nested_hkt_param_many_solutions_error.rs
@@ -1,7 +1,8 @@
-//check-fail
+// fail because there are too many solutions for I and L
+// check-fail
 fn test<I<%J>, L<%K>>(input: I<L<u32>>) {
 }
 
 fn main() {
-    test(Some(Some(5u32)));
+    test(Some(Some(5u32))); //~ ERROR too many possible types to infer HKT parameters [E10000]
 }
--- src/test/ui/hkt/inference_no_solution_error.rs
+++ src/test/ui/hkt/inference_no_solution_error.rs
@@ -1,7 +1,8 @@
-//check-fail
+// should fail because there should be no solution for hkt inference
+// check-fail
 fn test<I<%J>>(input: I<u32>, i2: I<bool>) {
 }
 
 fn main() {
-    test(Some(32u32), Some("dff".to_string()));
+    test(Some(32u32), Some("dff".to_string())); //~ ERROR cannot infer HKT parameters [E10001]
 }
